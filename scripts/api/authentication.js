2ba88c2ef103e09e22d29ae56f6cb0754cafb39a
Antonio Lopez <antonio.lopez2@studio.unibo.it>
2021-10-29 18:44:13 +0200

    Added try block in verify user

--- scripts/api/authentication.js
+++ scripts/api/authentication.js
@@ -127,24 +127,29 @@ function verifyLogin(req, res, next) {
  * @param {*} res Response object
  */
 function verifyUser(user, data, res) {
-    if (!user) {
-        res.status(404).json({ message: 'User not found' })
-    } else if (!bcrypt.compareSync(data.password || '', user.password)) {
-        res.status(403).json({ message: 'Wrong password' })
-    } else {
-        jwt.sign(
-            {
-                _id: user._id,
-                user: user.username,
-                role: user.role || 'customer',
-            },
-            privateKey,
-            { expiresIn: '1h', algorithm: 'RS256' },
-            (err, token) => {
-                if (err) res.status(500).json({ message: 'Internal error' })
-                else res.status(200).json({ accessToken: token })
-            }
-        )
+    try{
+        if (!user) {
+            res.status(404).json({ message: 'User not found' })
+        } else if (!bcrypt.compareSync(data.password || '', user.password)) {
+            res.status(403).json({ message: 'Wrong password' })
+        } else {
+            jwt.sign(
+                {
+                    _id: user._id,
+                    user: user.username,
+                    role: user.role || 'customer',
+                },
+                privateKey,
+                { expiresIn: '1h', algorithm: 'RS256' },
+                (err, token) => {
+                    if (err) res.status(500).json({ message: 'Internal error' })
+                    else res.status(200).json({ accessToken: token })
+                }
+            )
+        }
+    }
+    catch(err){
+        res.status(500).json({ message: err })
     }
 }
 
