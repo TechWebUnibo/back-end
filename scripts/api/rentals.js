6e0a417caa19b7f6abf0c7d35d43979c1f12f691
Antonio Lopez <antonio.lopez2@studio.unibo.it>
2021-12-05 18:06:27 +0100

    Added try catch to rent endpoint

--- scripts/api/rentals.js
+++ scripts/api/rentals.js
@@ -150,10 +150,15 @@ router.get('/', auth.verifyToken, async (req, res) => {
 })
 
 router.get('/:rentId', auth.verifyToken, async (req, res) => {
-    const rentId = req.params.rentId
-    let rent = await Rent.findById(rentId)
-    if (rent) return res.status(200).json(rent)
-    else return res.status(404).json({ message: 'Rent not found', error: {} })
+    try {
+        const rentId = req.params.rentId
+        let rent = await Rent.findById(rentId)
+        if (rent) return res.status(200).json(rent)
+        else return res.status(404).json({ message: 'Rent not found', error: {} })
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Server error', error: err })
+    }
 })
 
 router.post('/:rentId', auth.verifyToken, async (req, res) => {
